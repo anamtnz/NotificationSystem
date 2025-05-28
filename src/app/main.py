2a909f33c5d6e19c353cfced3190d66038e93626
Ana Martinez <anamtnztorres@gmail.com>
2025-05-28 20:07:33 +0200

    ADD: initial structure

---
+++ src/app/main.py
@@ -0,0 +1,17 @@
+import uvicorn
+from fastapi import FastAPI
+
+from app.routers.publisher import publisher
+from app.routers.subscriber import subscriber
+
+app = FastAPI()
+app.include_router(subscriber.router)
+app.include_router(publisher.router)
+
+
+def main():
+    uvicorn.run("main:app", host="0.0.0.0", port=8000)
+
+
+if __name__ == "__main__":
+    main()
---
+++ src/app/publisher/publisher_manager.py
@@ -0,0 +1,2 @@
+class PublishersManager:
+    pass
