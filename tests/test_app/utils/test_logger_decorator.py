a51b9af80b1190d74fc36780944e97affd19f50f
Ana Martinez <anamtnztorres@gmail.com>
2025-05-30 19:33:47 +0200

    ADD: logger decorator tests

---
+++ tests/test_app/utils/test_logger_decorator.py
@@ -0,0 +1,35 @@
+from logging import Logger
+from unittest.mock import patch, call
+
+import pytest
+
+from app.utils.logger_decorator import logger_decorator
+
+
+@patch.object(Logger, 'info')
+@patch.object(Logger, 'exception')
+def test_logger_decorator(exception_mock, info_mock):
+    @logger_decorator
+    def function_to_decorate():
+        pass
+
+    function_to_decorate()
+
+    info_mock.assert_has_calls(
+        [call("Starting function function_to_decorate"), call("Finishing function function_to_decorate")])
+    exception_mock.assert_not_called()
+
+
+@patch.object(Logger, 'info')
+@patch.object(Logger, 'exception')
+def test_logger_decorator_exception(exception_mock, info_mock):
+    @logger_decorator
+    def function_to_decorate():
+        raise Exception
+
+    with pytest.raises(Exception):
+        function_to_decorate()
+
+    info_mock.assert_has_calls(
+        [call("Starting function function_to_decorate"), call("Finishing function function_to_decorate")])
+    exception_mock.assert_called_once_with("Exception executing function function_to_decorate: ")
